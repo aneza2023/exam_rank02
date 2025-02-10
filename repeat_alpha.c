725b1b7f5b39f599af13ef8c554f642c3b9f3672
aneza2023 <163205385+aneza2023@users.noreply.github.com>
2025-02-10 11:39:01 +0000

    repeat alpha only if abc

--- repeat_alpha.c
+++ repeat_alpha.c
@@ -2,7 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int repeat_alpha(char *str)
+{
+    int i;
+    int k;
+    int j;
+    int count;
+    int repeat;
+
+    i = 0;
+    k = 65;
+    j = 97;
+    count = 1;
+    while (str[i] != '\0')
+    {
+        repeat = count;
+        if (str[i] == k || str[i] == j)
+        {
+            while (repeat > 0){
+                write (1, &str[i], 1);
+                repeat--;
+            }
+            
+        }
+        i++;
+        j++;
+        k++;
+        count++;
+    }
+    return (0);
+}
+
 int main(void)
 {
-    
+    char *str;
+
+    str = "adel";
+    repeat_alpha(str);
+    return (0);
 }
