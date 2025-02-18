bb02d67f60aec52a1134a3b56ac0b8018574a241
aneza2023 <163205385+aneza2023@users.noreply.github.com>
2025-02-18 14:33:01 +0000

    expandstr

---
+++ level03/expand_str.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_break(char *argv)
+{
+    int i;
+
+    i = 0;
+    while(argv[i] == 32 || argv[i] == 9)
+    {
+        i++;
+    }
+    return(i);
+}
+
+int expand_str(char *str)
+{
+    int i;
+    int k;
+
+    i = 0;
+    k = ft_break(str);
+    while (str[i] != '\0')
+    {
+        if (str[i] != 32 && str[i] != 9)
+            write (1, &str[i], 1);
+        else if (((i > k) && str[i + 1] != '\0') && (str[i] == 32 || str[i] == 9) && (str[i + 1] != 32 && str[i + 1] != 9))
+        {
+            write (1, &str[i], 1);
+            write (1, &str[i], 1);
+            write (1, &str[i], 1);
+        }
+        i++;
+    }
+    return (0);
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2 || argc > 2)
+        write (1, "\n", 2);
+    else
+        expand_str(argv[1]);
+    return(0);
+}
